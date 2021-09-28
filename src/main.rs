dd9e565ca4e0e17aeb5cc4a317c0b71a5b060f54
view <view@g.com>
2021-09-28 04:55:20 -0700

    .

--- src/main.rs
+++ src/main.rs
@@ -168,175 +168,169 @@ extern "C" fn kinit() {
     // Free the testing buffer.
     kmem::kfree(buffer);
 
+    // Map heap allocations
+    let root_ptr = kmem::get_page_table();
+    let root_u = root_ptr as usize;
+    let mut root = unsafe { root_ptr.as_mut().unwrap() };
+    let kheap_head = kmem::get_head() as usize;
+    let total_pages = kmem::get_num_allocations();
+    println!();
+    println!();
+    unsafe {
+        println!("TEXT:   0x{:x} -> 0x{:x}", TEXT_START, TEXT_END);
+        println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
+        println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
+        println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
+        println!(
+            "STACK:  0x{:x} -> 0x{:x}",
+            KERNEL_STACK_START, KERNEL_STACK_END
+        );
+        println!(
+            "HEAP:   0x{:x} -> 0x{:x}",
+            kheap_head,
+            kheap_head + total_pages * page::PAGE_SIZE
+        );
+    }
+    id_map_range(
+        &mut root,
+        kheap_head,
+        kheap_head + total_pages * page::PAGE_SIZE,
+        page::EntryBits::ReadWrite.val(),
+    );
+    // Using statics is inherently unsafe.
+    unsafe {
+        // Map heap descriptors
+        let num_pages = HEAP_SIZE / page::PAGE_SIZE;
+        id_map_range(
+            &mut root,
+            HEAP_START,
+            HEAP_START + num_pages,
+            page::EntryBits::ReadWrite.val(),
+        );
+        // Map executable section
+        id_map_range(
+            &mut root,
+            TEXT_START,
+            TEXT_END,
+            page::EntryBits::ReadExecute.val(),
+        );
+        // Map rodata section
+        // We put the ROdata section into the text section, so they can
+        // potentially overlap however, we only care that it's read
+        // only.
+        id_map_range(
+            &mut root,
+            RODATA_START,
+            RODATA_END,
+            page::EntryBits::ReadExecute.val(),
+        );
+        // Map data section
+        id_map_range(
+            &mut root,
+            DATA_START,
+            DATA_END,
+            page::EntryBits::ReadWrite.val(),
+        );
+        // Map bss section
+        id_map_range(
+            &mut root,
+            BSS_START,
+            BSS_END,
+            page::EntryBits::ReadWrite.val(),
+        );
+        // Map kernel stack
+        id_map_range(
+            &mut root,
+            KERNEL_STACK_START,
+            KERNEL_STACK_END,
+            page::EntryBits::ReadWrite.val(),
+        );
+    }
 
-	// Map heap allocations
-	let root_ptr = kmem::get_page_table();
-	let root_u = root_ptr as usize;
-	let mut root = unsafe { root_ptr.as_mut().unwrap() };
-	let kheap_head = kmem::get_head() as usize;
-	let total_pages = kmem::get_num_allocations();
-	println!();
-	println!();
-	unsafe {
-		println!("TEXT:   0x{:x} -> 0x{:x}", TEXT_START, TEXT_END);
-		println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
-		println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
-		println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
-		println!(
-		         "STACK:  0x{:x} -> 0x{:x}",
-		         KERNEL_STACK_START, KERNEL_STACK_END
-		);
-		println!(
-		         "HEAP:   0x{:x} -> 0x{:x}",
-		         kheap_head,
-		         kheap_head + total_pages * page::PAGE_SIZE
-		);
-	}
-	id_map_range(
-	             &mut root,
-	             kheap_head,
-	             kheap_head + total_pages * page::PAGE_SIZE,
-	             page::EntryBits::ReadWrite.val(),
-	);
-	// Using statics is inherently unsafe.
-	unsafe {
-		// Map heap descriptors
-		let num_pages = HEAP_SIZE / page::PAGE_SIZE;
-		id_map_range(
-		             &mut root,
-		             HEAP_START,
-		             HEAP_START + num_pages,
-		             page::EntryBits::ReadWrite.val(),
-		);
-		// Map executable section
-		id_map_range(
-		             &mut root,
-		             TEXT_START,
-		             TEXT_END,
-		             page::EntryBits::ReadExecute.val(),
-		);
-		// Map rodata section
-		// We put the ROdata section into the text section, so they can
-		// potentially overlap however, we only care that it's read
-		// only.
-		id_map_range(
-		             &mut root,
-		             RODATA_START,
-		             RODATA_END,
-		             page::EntryBits::ReadExecute.val(),
-		);
-		// Map data section
-		id_map_range(
-		             &mut root,
-		             DATA_START,
-		             DATA_END,
-		             page::EntryBits::ReadWrite.val(),
-		);
-		// Map bss section
-		id_map_range(
-		             &mut root,
-		             BSS_START,
-		             BSS_END,
-		             page::EntryBits::ReadWrite.val(),
-		);
-		// Map kernel stack
-		id_map_range(
-		             &mut root,
-		             KERNEL_STACK_START,
-		             KERNEL_STACK_END,
-		             page::EntryBits::ReadWrite.val(),
-		);
-	}
+    // UART
+    id_map_range(
+        &mut root,
+        0x1000_0000,
+        0x1000_0100,
+        page::EntryBits::ReadWrite.val(),
+    );
 
-	// UART
-	id_map_range(
-	             &mut root,
-	             0x1000_0000,
-	             0x1000_0100,
-	             page::EntryBits::ReadWrite.val(),
-	);
+    // CLINT
+    //  -> MSIP
+    id_map_range(
+        &mut root,
+        0x0200_0000,
+        0x0200_ffff,
+        page::EntryBits::ReadWrite.val(),
+    );
+    // PLIC
+    id_map_range(
+        &mut root,
+        0x0c00_0000,
+        0x0c00_2000,
+        page::EntryBits::ReadWrite.val(),
+    );
+    id_map_range(
+        &mut root,
+        0x0c20_0000,
+        0x0c20_8000,
+        page::EntryBits::ReadWrite.val(),
+    );
+    // When we return from here, we'll go back to boot.S and switch into
+    // supervisor mode We will return the SATP register to be written when
+    // we return. root_u is the root page table's address. When stored into
+    // the SATP register, this is divided by 4 KiB (right shift by 12 bits).
+    // We enable the MMU by setting mode 8. Bits 63, 62, 61, 60 determine
+    // the mode.
+    // 0 = Bare (no translation)
+    // 8 = Sv39
+    // 9 = Sv48
+    // build_satp has these parameters: mode, asid, page table address.
+    let satp_value = cpu::build_satp(cpu::SatpMode::Sv39, 0, root_u);
+    unsafe {
+        // We have to store the kernel's table. The tables will be moved
+        // back and forth between the kernel's table and user
+        // applicatons' tables. Note that we're writing the physical address
+        // of the trap frame.
+        cpu::mscratch_write((&mut cpu::KERNEL_TRAP_FRAME[0] as *mut cpu::TrapFrame) as usize);
+        cpu::sscratch_write(cpu::mscratch_read());
+        cpu::KERNEL_TRAP_FRAME[0].satp = satp_value;
+        // Move the stack pointer to the very bottom. The stack is
+        // actually in a non-mapped page. The stack is decrement-before
+        // push and increment after pop. Therefore, the stack will be
+        // allocated (decremented) before it is stored.
+        // cpu::KERNEL_TRAP_FRAME[0].trap_stack =
+        // 	page::zalloc(1).add(page::PAGE_SIZE);
+        // id_map_range(
+        //              &mut root,
+        //              cpu::KERNEL_TRAP_FRAME[0].trap_stack
+        //                                       .sub(page::PAGE_SIZE,)
+        //              as usize,
+        //              cpu::KERNEL_TRAP_FRAME[0].trap_stack as usize,
+        //              page::EntryBits::ReadWrite.val(),
+        // );
+        // The trap frame itself is stored in the mscratch register.
+        id_map_range(
+            &mut root,
+            cpu::mscratch_read(),
+            cpu::mscratch_read() + core::mem::size_of::<cpu::TrapFrame>(),
+            page::EntryBits::ReadWrite.val(),
+        );
+        page::print_page_allocations();
+        // let p = cpu::KERNEL_TRAP_FRAME[0].trap_stack as usize - 1;
+        // let m = page::walk(&root, p).unwrap_or(0);
+        // println!("Walk 0x{:x} = 0x{:x}", p, m);
+    }
+    // The following shows how we're going to walk to translate a virtual
+    // address into a physical address. We will use this whenever a user
+    // space application requires services. Since the user space application
+    // only knows virtual addresses, we have to translate silently behind
+    // the scenes.
+    println!("Setting 0x{:x}", satp_value);
+    println!("Scratch reg = 0x{:x}", cpu::mscratch_read());
 
-	// CLINT
-	//  -> MSIP
-	id_map_range(
-	             &mut root,
-	             0x0200_0000,
-	             0x0200_ffff,
-	             page::EntryBits::ReadWrite.val(),
-	);
-	// PLIC
-	id_map_range(
-	             &mut root,
-	             0x0c00_0000,
-	             0x0c00_2000,
-	             page::EntryBits::ReadWrite.val(),
-	);
-	id_map_range(
-	             &mut root,
-	             0x0c20_0000,
-	             0x0c20_8000,
-	             page::EntryBits::ReadWrite.val(),
-	);
-	// When we return from here, we'll go back to boot.S and switch into
-	// supervisor mode We will return the SATP register to be written when
-	// we return. root_u is the root page table's address. When stored into
-	// the SATP register, this is divided by 4 KiB (right shift by 12 bits).
-	// We enable the MMU by setting mode 8. Bits 63, 62, 61, 60 determine
-	// the mode.
-	// 0 = Bare (no translation)
-	// 8 = Sv39
-	// 9 = Sv48
-	// build_satp has these parameters: mode, asid, page table address.
-	let satp_value = cpu::build_satp(cpu::SatpMode::Sv39, 0, root_u);
-	unsafe {
-		// We have to store the kernel's table. The tables will be moved
-		// back and forth between the kernel's table and user
-		// applicatons' tables. Note that we're writing the physical address
-		// of the trap frame.
-		cpu::mscratch_write(
-		                    (&mut cpu::KERNEL_TRAP_FRAME[0]
-		                     as *mut cpu::TrapFrame)
-		                    as usize,
-		);
-		cpu::sscratch_write(cpu::mscratch_read());
-		cpu::KERNEL_TRAP_FRAME[0].satp = satp_value;
-		// Move the stack pointer to the very bottom. The stack is
-		// actually in a non-mapped page. The stack is decrement-before
-		// push and increment after pop. Therefore, the stack will be
-		// allocated (decremented) before it is stored.
-		// cpu::KERNEL_TRAP_FRAME[0].trap_stack =
-		// 	page::zalloc(1).add(page::PAGE_SIZE);
-		// id_map_range(
-		//              &mut root,
-		//              cpu::KERNEL_TRAP_FRAME[0].trap_stack
-		//                                       .sub(page::PAGE_SIZE,)
-		//              as usize,
-		//              cpu::KERNEL_TRAP_FRAME[0].trap_stack as usize,
-		//              page::EntryBits::ReadWrite.val(),
-		// );
-		// The trap frame itself is stored in the mscratch register.
-		id_map_range(
-		             &mut root,
-		             cpu::mscratch_read(),
-		             cpu::mscratch_read()
-		             + core::mem::size_of::<cpu::TrapFrame,>(),
-		             page::EntryBits::ReadWrite.val(),
-		);
-		page::print_page_allocations();
-		// let p = cpu::KERNEL_TRAP_FRAME[0].trap_stack as usize - 1;
-		// let m = page::walk(&root, p).unwrap_or(0);
-		// println!("Walk 0x{:x} = 0x{:x}", p, m);
-	}
-	// The following shows how we're going to walk to translate a virtual
-	// address into a physical address. We will use this whenever a user
-	// space application requires services. Since the user space application
-	// only knows virtual addresses, we have to translate silently behind
-	// the scenes.
-	println!("Setting 0x{:x}", satp_value);
-	println!("Scratch reg = 0x{:x}", cpu::mscratch_read());
-
-	cpu::satp_write(satp_value);
-	cpu::satp_fence_asid(0);
+    cpu::satp_write(satp_value);
+    cpu::satp_fence_asid(0);
 
     // We schedule the next context switch using a multiplier of 1
     // trap::schedule_next_context_switch(1);
@@ -430,13 +424,12 @@ extern "C" fn kmain() {
     // plic::enable(10);
     // plic::set_priority(10, 1);
 
-
     // trap::schedule_next_context_switch(1);
     // let frame_addr=sched::schedule();
     // rust_switch_to_user(frame_addr);
 
     println!("kmain over");
-    loop{}
+    loop {}
 
     // println!("UART interrupts have been enabled and are awaiting your command");
 }
--- src/process.rs
+++ src/process.rs
@@ -1,14 +1,9 @@
 use core::fmt::Write;
 
-use crate::{cpu::{TrapFrame, satp_fence_asid, build_satp, SatpMode},
-            page::{alloc,
-                   dealloc,
-                   map,
-                   unmap,
-                   zalloc,
-                   EntryBits,
-                   Table,
-                   PAGE_SIZE}};
+use crate::{
+    cpu::{build_satp, satp_fence_asid, SatpMode, TrapFrame},
+    page::{alloc, dealloc, map, unmap, zalloc, EntryBits, Table, PAGE_SIZE},
+};
 use alloc::collections::vec_deque::VecDeque;
 
 // How many pages are we going to give a process for their
@@ -36,78 +31,78 @@ pub static mut PROCESS_LIST: Option<VecDeque<Process>> = None;
 static mut NEXT_PID: u16 = 1;
 
 extern "C" {
-	fn make_syscall(a: usize) -> usize;
+    fn make_syscall(a: usize) -> usize;
 }
 
 /// We will eventually move this function out of here, but its
 /// job is just to take a slot in the process list.
 fn init_process() {
-	// We can't do much here until we have system calls because
-	// we're running in User space.
-	let mut i: usize = 0;
-	loop {
-		i += 1;
-		if i > 100_000_000 {
-			unsafe {
-				make_syscall(1);
-			}
-			i = 0;
-		}
-	}
+    // We can't do much here until we have system calls because
+    // we're running in User space.
+    let mut i: usize = 0;
+    loop {
+        i += 1;
+        if i > 100_000_000 {
+            unsafe {
+                make_syscall(1);
+            }
+            i = 0;
+        }
+    }
 }
 
 /// Add a process given a function address and then
 /// push it onto the LinkedList. Uses Process::new_default
 /// to create a new stack, etc.
 pub fn add_process_default(pr: fn()) {
-	unsafe {
-		// This is the Rust-ism that really trips up C++ programmers.
-		// PROCESS_LIST is wrapped in an Option<> enumeration, which
-		// means that the Option owns the Deque. We can only borrow from
-		// it or move ownership to us. In this case, we choose the
-		// latter, where we move ownership to us, add a process, and
-		// then move ownership back to the PROCESS_LIST.
-		// This allows mutual exclusion as anyone else trying to grab
-		// the process list will get None rather than the Deque.
-		if let Some(mut pl) = PROCESS_LIST.take() {
-			// .take() will replace PROCESS_LIST with None and give
-			// us the only copy of the Deque.
-			let p = Process::new_default(pr);
-			pl.push_back(p);
-			// Now, we no longer need the owned Deque, so we hand it
-			// back by replacing the PROCESS_LIST's None with the
-			// Some(pl).
-			PROCESS_LIST.replace(pl);
-		}
-		// TODO: When we get to multi-hart processing, we need to keep
-		// trying to grab the process list. We can do this with an
-		// atomic instruction. but right now, we're a single-processor
-		// computer.
-	}
+    unsafe {
+        // This is the Rust-ism that really trips up C++ programmers.
+        // PROCESS_LIST is wrapped in an Option<> enumeration, which
+        // means that the Option owns the Deque. We can only borrow from
+        // it or move ownership to us. In this case, we choose the
+        // latter, where we move ownership to us, add a process, and
+        // then move ownership back to the PROCESS_LIST.
+        // This allows mutual exclusion as anyone else trying to grab
+        // the process list will get None rather than the Deque.
+        if let Some(mut pl) = PROCESS_LIST.take() {
+            // .take() will replace PROCESS_LIST with None and give
+            // us the only copy of the Deque.
+            let p = Process::new_default(pr);
+            pl.push_back(p);
+            // Now, we no longer need the owned Deque, so we hand it
+            // back by replacing the PROCESS_LIST's None with the
+            // Some(pl).
+            PROCESS_LIST.replace(pl);
+        }
+        // TODO: When we get to multi-hart processing, we need to keep
+        // trying to grab the process list. We can do this with an
+        // atomic instruction. but right now, we're a single-processor
+        // computer.
+    }
 }
 
 /// This should only be called once, and its job is to create
 /// the init process. Right now, this process is in the kernel,
 /// but later, it should call the shell.
 pub fn init() -> usize {
-	unsafe {
-		PROCESS_LIST = Some(VecDeque::with_capacity(15));
-		add_process_default(init_process);
-		// Ugh....Rust is giving me fits over here!
-		// I just want a memory address to the trap frame, but
-		// due to the borrow rules of Rust, I'm fighting here. So,
-		// instead, let's move the value out of PROCESS_LIST, get
-		// the address, and then move it right back in.
-		let pl = PROCESS_LIST.take().unwrap();
-		let p = pl.front().unwrap().frame;
-		// let frame = p as *const TrapFrame as usize;
-		// println!("Init's frame is at 0x{:08x}", frame);
-		// Put the process list back in the global.
-		PROCESS_LIST.replace(pl);
-		// Return the first instruction's address to execute.
-		// Since we use the MMU, all start here.
-		(*p).pc
-	}
+    unsafe {
+        PROCESS_LIST = Some(VecDeque::with_capacity(15));
+        add_process_default(init_process);
+        // Ugh....Rust is giving me fits over here!
+        // I just want a memory address to the trap frame, but
+        // due to the borrow rules of Rust, I'm fighting here. So,
+        // instead, let's move the value out of PROCESS_LIST, get
+        // the address, and then move it right back in.
+        let pl = PROCESS_LIST.take().unwrap();
+        let p = pl.front().unwrap().frame;
+        // let frame = p as *const TrapFrame as usize;
+        // println!("Init's frame is at 0x{:08x}", frame);
+        // Put the process list back in the global.
+        PROCESS_LIST.replace(pl);
+        // Return the first instruction's address to execute.
+        // Since we use the MMU, all start here.
+        (*p).pc
+    }
 }
 
 // Our process must be able to sleep, wait, or run.
@@ -118,10 +113,10 @@ pub fn init() -> usize {
 //        it out of the list later.
 #[repr(u8)]
 pub enum ProcessState {
-	Running,
-	Sleeping,
-	Waiting,
-	Dead,
+    Running,
+    Sleeping,
+    Waiting,
+    Dead,
 }
 
 // Let's represent this in C ABI. We do this
@@ -131,123 +126,133 @@ pub enum ProcessState {
 // C-style ABI.
 #[repr(C)]
 pub struct Process {
-	frame:           *mut TrapFrame,
-	stack:           *mut u8,
-	pid:             u16,
-	root:            *mut Table,
-	state:           ProcessState,
-	data:            ProcessData,
-	sleep_until:	 usize,
+    frame: *mut TrapFrame,
+    stack: *mut u8,
+    pid: u16,
+    root: *mut Table,
+    state: ProcessState,
+    data: ProcessData,
+    sleep_until: usize,
 }
 
 impl Process {
-	pub fn get_frame_address(&self) -> usize {
-		self.frame as usize
-	}
-	pub fn get_program_counter(&self) -> usize {
-		unsafe { (*self.frame).pc }
-	}
-	pub fn get_table_address(&self) -> usize {
-		self.root as usize
-	}
-	pub fn get_state(&self) -> &ProcessState {
-		&self.state
-	}
-	pub fn get_pid(&self) -> u16 {
-		self.pid
-	}
-	pub fn get_sleep_until(&self) -> usize {
-		self.sleep_until
-	}
-	pub fn new_default(func: fn()) -> Self {
-		let func_addr = func as usize;
-		let func_vaddr = func_addr; //- 0x6000_0000;
-		// println!("func_addr = {:x} -> {:x}", func_addr, func_vaddr);
-		// We will convert NEXT_PID below into an atomic increment when
-		// we start getting into multi-hart processing. For now, we want
-		// a process. Get it to work, then improve it!
-		let mut ret_proc =
-			Process { frame:           zalloc(1) as *mut TrapFrame,
-			          stack:           alloc(STACK_PAGES),
-			          pid:             unsafe { NEXT_PID },
-			          root:            zalloc(1) as *mut Table,
-			          state:           ProcessState::Running,
-					  data:            ProcessData::zero(), 
-					  sleep_until:     0
-					};
-		unsafe {
-			satp_fence_asid(NEXT_PID as usize);
-			NEXT_PID += 1;
-		}
-		// Now we move the stack pointer to the bottom of the
-		// allocation. The spec shows that register x2 (2) is the stack
-		// pointer.
-		// We could use ret_proc.stack.add, but that's an unsafe
-		// function which would require an unsafe block. So, convert it
-		// to usize first and then add PAGE_SIZE is better.
-		// We also need to set the stack adjustment so that it is at the
-		// bottom of the memory and far away from heap allocations.
-		let saddr = ret_proc.stack as usize;
-		unsafe {
-			(*ret_proc.frame).pc = func_vaddr;
-			(*ret_proc.frame).regs[2] = STACK_ADDR + PAGE_SIZE * STACK_PAGES;
-		}
-		// Map the stack on the MMU
-		let pt;
-		unsafe {
-			pt = &mut *ret_proc.root;
-			(*ret_proc.frame).satp = build_satp(SatpMode::Sv39, ret_proc.pid as usize, ret_proc.root as usize);
-		}
-		// We need to map the stack onto the user process' virtual
-		// memory This gets a little hairy because we need to also map
-		// the function code too.
-		for i in 0..STACK_PAGES {
-			let addr = i * PAGE_SIZE;
-			map(
-			    pt,
-			    STACK_ADDR + addr,
-			    saddr + addr,
-			    EntryBits::UserReadWrite.val(),
-			    0,
-			);
-			// println!("Set stack from 0x{:016x} -> 0x{:016x}", STACK_ADDR + addr, saddr + addr);
-		}
-		// Map the program counter on the MMU and other bits
-		for i in 0..=100 {
-			let modifier = i * 0x1000;
-			map(
-				pt,
-				func_vaddr + modifier,
-				func_addr + modifier,
-				EntryBits::UserReadWriteExecute.val(),
-				0,
-			);
-		}
-		// This is the make_syscall function
-		// The reason we need this is because we're running a process
-		// that is inside of the kernel. When we start loading from a block
-		// devices, we can load the instructions anywhere in memory. 
-		map(pt, 0x8000_0000, 0x8000_0000, EntryBits::UserReadExecute.val(), 0);
-		ret_proc
-	}
+    pub fn get_frame_address(&self) -> usize {
+        self.frame as usize
+    }
+    pub fn get_program_counter(&self) -> usize {
+        unsafe { (*self.frame).pc }
+    }
+    pub fn get_table_address(&self) -> usize {
+        self.root as usize
+    }
+    pub fn get_state(&self) -> &ProcessState {
+        &self.state
+    }
+    pub fn get_pid(&self) -> u16 {
+        self.pid
+    }
+    pub fn get_sleep_until(&self) -> usize {
+        self.sleep_until
+    }
+    pub fn new_default(func: fn()) -> Self {
+        let func_addr = func as usize;
+        let func_vaddr = func_addr; //- 0x6000_0000;
+                                    // println!("func_addr = {:x} -> {:x}", func_addr, func_vaddr);
+                                    // We will convert NEXT_PID below into an atomic increment when
+                                    // we start getting into multi-hart processing. For now, we want
+                                    // a process. Get it to work, then improve it!
+        let mut ret_proc = Process {
+            frame: zalloc(1) as *mut TrapFrame,
+            stack: alloc(STACK_PAGES),
+            pid: unsafe { NEXT_PID },
+            root: zalloc(1) as *mut Table,
+            state: ProcessState::Running,
+            data: ProcessData::zero(),
+            sleep_until: 0,
+        };
+        unsafe {
+            satp_fence_asid(NEXT_PID as usize);
+            NEXT_PID += 1;
+        }
+        // Now we move the stack pointer to the bottom of the
+        // allocation. The spec shows that register x2 (2) is the stack
+        // pointer.
+        // We could use ret_proc.stack.add, but that's an unsafe
+        // function which would require an unsafe block. So, convert it
+        // to usize first and then add PAGE_SIZE is better.
+        // We also need to set the stack adjustment so that it is at the
+        // bottom of the memory and far away from heap allocations.
+        let saddr = ret_proc.stack as usize;
+        unsafe {
+            (*ret_proc.frame).pc = func_vaddr;
+            (*ret_proc.frame).regs[2] = STACK_ADDR + PAGE_SIZE * STACK_PAGES;
+        }
+        // Map the stack on the MMU
+        let pt;
+        unsafe {
+            pt = &mut *ret_proc.root;
+            (*ret_proc.frame).satp = build_satp(
+                SatpMode::Sv39,
+                ret_proc.pid as usize,
+                ret_proc.root as usize,
+            );
+        }
+        // We need to map the stack onto the user process' virtual
+        // memory This gets a little hairy because we need to also map
+        // the function code too.
+        for i in 0..STACK_PAGES {
+            let addr = i * PAGE_SIZE;
+            map(
+                pt,
+                STACK_ADDR + addr,
+                saddr + addr,
+                EntryBits::UserReadWrite.val(),
+                0,
+            );
+            // println!("Set stack from 0x{:016x} -> 0x{:016x}", STACK_ADDR + addr, saddr + addr);
+        }
+        // Map the program counter on the MMU and other bits
+        for i in 0..=100 {
+            let modifier = i * 0x1000;
+            map(
+                pt,
+                func_vaddr + modifier,
+                func_addr + modifier,
+                EntryBits::UserReadWriteExecute.val(),
+                0,
+            );
+        }
+        // This is the make_syscall function
+        // The reason we need this is because we're running a process
+        // that is inside of the kernel. When we start loading from a block
+        // devices, we can load the instructions anywhere in memory.
+        map(
+            pt,
+            0x8000_0000,
+            0x8000_0000,
+            EntryBits::UserReadExecute.val(),
+            0,
+        );
+        ret_proc
+    }
 }
 
 impl Drop for Process {
-	/// Since we're storing ownership of a Process in the linked list,
-	/// we can cause it to deallocate automatically when it is removed.
-	fn drop(&mut self) {
-		// We allocate the stack as a page.
-		dealloc(self.stack);
-		// This is unsafe, but it's at the drop stage, so we won't
-		// be using this again.
-		unsafe {
-			// Remember that unmap unmaps all levels of page tables
-			// except for the root. It also deallocates the memory
-			// associated with the tables.
-			unmap(&mut *self.root);
-		}
-		dealloc(self.root as *mut u8);
-	}
+    /// Since we're storing ownership of a Process in the linked list,
+    /// we can cause it to deallocate automatically when it is removed.
+    fn drop(&mut self) {
+        // We allocate the stack as a page.
+        dealloc(self.stack);
+        // This is unsafe, but it's at the drop stage, so we won't
+        // be using this again.
+        unsafe {
+            // Remember that unmap unmaps all levels of page tables
+            // except for the root. It also deallocates the memory
+            // associated with the tables.
+            unmap(&mut *self.root);
+        }
+        dealloc(self.root as *mut u8);
+    }
 }
 
 // The private data in a process contains information
@@ -257,14 +262,14 @@ impl Drop for Process {
 // private process data. This is essentially our resource control block (RCB).
 #[allow(dead_code)]
 pub struct ProcessData {
-	cwd_path: [u8; 128],
+    cwd_path: [u8; 128],
 }
 
 // This is private data that we can query with system calls.
 // If we want to implement CFQ (completely fair queuing), which
 // is a per-process block queuing algorithm, we can put that here.
 impl ProcessData {
-	pub fn zero() -> Self {
-		ProcessData { cwd_path: [0; 128], }
-	}
+    pub fn zero() -> Self {
+        ProcessData { cwd_path: [0; 128] }
+    }
 }
